7f54ebe2f5805a9510440b45a9f9527461333299
Lotfy <sl185199@ncr.com>
2022-09-20 20:01:53 +0200

    Make slight modification in the code to make it look decent in addition to adding comments

--- Shady Ahmed - B00881842 - Fizzbuzz Quiz 1 answer .py
+++ Shady Ahmed - B00881842 - Fizzbuzz Quiz 1 answer .py
@@ -3,11 +3,11 @@
 
 # Initial time range values
 X_first = int(1)  
-X_last = int(101)
+X_last = int(101)   #Make the final count to 101
 
-def FizzBuzzHandler(first,last):
+def FizzBuzzHandler(first,last):        # Create a function to handle FizzBuzz code
     
-    for v in range(first,last):
+    for v in range(first,last): #For that include an if condition 
         if v %3 ==0 and v%5 ==0:
             print("FizzBuzz")
         elif v%3 == 0:
@@ -16,7 +16,7 @@ def FizzBuzzHandler(first,last):
             print("Buzz")
         else:
             print(int(v))
-    return()
 
-FizzBuzzHandler(X_first,X_last)
+print("FizzBuzz Game starting to work from" , X_first, "to", X_last)    #Making the code look fancy and decent :)
+FizzBuzzHandler(X_first,X_last) # Calling the function
             
