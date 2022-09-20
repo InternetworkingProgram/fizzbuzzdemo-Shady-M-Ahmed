72579b098b8ea0934a82b5726bd2d316336c5c91
Lotfy <sl185199@ncr.com>
2022-09-20 19:56:21 +0200

    Uploading the code file after testing

--- Shady Ahmed - B00881842 - Fizzbuzz Quiz 1 answer .py
+++ Shady Ahmed - B00881842 - Fizzbuzz Quiz 1 answer .py
@@ -1,6 +1,22 @@
 
 # Making Initial code design
 
-X_first = 1
-X_last = 100
+# Initial time range values
+X_first = int(1)  
+X_last = int(101)
 
+def FizzBuzzHandler(first,last):
+    
+    for v in range(first,last):
+        if v %3 ==0 and v%5 ==0:
+            print("FizzBuzz")
+        elif v%3 == 0:
+            print("Fizz")
+        elif v%5 == 0:
+            print("Buzz")
+        else:
+            print(int(v))
+    return()
+
+FizzBuzzHandler(X_first,X_last)
+            
